597e1ff92d78f74788e5f027ce98aa68e9278563
krishore-kumar <krrish3rakholiya@gmail.com>
2023-11-26 19:54:53 +0530

    fd

--- phase1/trader.cpp
+++ phase1/trader.cpp
@@ -1,10 +1,25 @@
 #include "receiver.h"
+using namespace std;
 
-int main() {
+int main(int argc , char* argv[]) {
 
     Receiver rcv;
     sleep(5);
     std::string message = rcv.readIML();
     std::cout << message;
+
+    if(stoi(argv[1]) == 1){
+        //part1
+    }
+
+    if(stoi(argv[1]) == 2){
+        //part2
+    }
+
+    if(stoi(argv[1]) == 3){
+        //part3
+    }
+
+
     return 0;
 }
